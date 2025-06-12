9792f2366df4ca66f4f5e88a449bf4cd3e75d087
Aleena <aleenaghafoor239@gmail.com>
2025-06-12 22:47:37 +0500

    bugs resolve

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@react-router/dev/config"
 
 export default {
+	serverModuleFormat: "esm",
 	future: {
 		unstable_viteEnvironmentApi: true,
 		unstable_splitRouteModules: true,

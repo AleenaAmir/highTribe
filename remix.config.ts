01dee85ade179d0bb58030400a3969ff32fcecfe
Aleena <aleenaghafoor239@gmail.com>
2025-06-12 17:48:20 +0500

    remix config update

--- remix.config.ts
+++ remix.config.ts
@@ -10,18 +10,4 @@ export default {
 		v3_relativeSplatPath: true,
 	},
 	// other config...
-	routes(defineRoute) {
-		return defineRoute((route) => {
-			route.use("_index", "routes/_index.tsx");
-			route.use("login", "routes/login.tsx");
-			route.use("signup", "routes/signup.tsx");
-			route.use("api.auth", "routes/api.auth.ts");
-			route.use("api.auth.login", "routes/api.auth.login.ts");
-			route.use("api.users", "routes/api.users.ts");
-			route.use("resource.locales", "routes/resource.locales.ts");
-			route.use("robots.txt", "routes/robots[.]txt.ts");
-			route.use("sitemap-index.xml", "routes/sitemap-index[.]xml.ts");
-			route.use("sitemap.$lang.xml", "routes/sitemap.$lang[.]xml.ts");
-		});
-	}
 }

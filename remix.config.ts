dffdcbbbeb4fbdc7b4cb4e34413c1d0fdaaa0b86
Aleena <aleenaghafoor239@gmail.com>
2025-06-12 17:37:34 +0500

    remix config update

--- remix.config.ts
+++ remix.config.ts
@@ -1,9 +1,19 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-	server: "./server/index.ts", // this uses Hono as entry
+	server: "./app/server/index.ts", // this uses Hono as entry
 	// serverBuildTarget: "vercel",
 	// or for edge:
 	serverBuildTarget: "vercel-edge",
 	ignoredRouteFiles: ["**/.*"],
+	future: {
+		v3_fetcherPersist: true,
+		v3_relativeSplatPath: true,
+	},
 	// other config...
+	routes(defineRoute) {
+		return defineRoute((route) => {
+			route.use("_index", "routes/_index.tsx");
+			// Add other routes here
+		});
+	}
 }

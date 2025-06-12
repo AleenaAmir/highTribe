28c83f33e3deebeb2c37706e6105e8f9f243d1d3
Aleena <aleenaghafoor239@gmail.com>
2025-06-12 17:43:10 +0500

    remix config update

--- remix.config.ts
+++ remix.config.ts
@@ -13,7 +13,15 @@ export default {
 	routes(defineRoute) {
 		return defineRoute((route) => {
 			route.use("_index", "routes/_index.tsx");
-			// Add other routes here
+			route.use("login", "routes/login.tsx");
+			route.use("signup", "routes/signup.tsx");
+			route.use("api.auth", "routes/api.auth.ts");
+			route.use("api.auth.login", "routes/api.auth.login.ts");
+			route.use("api.users", "routes/api.users.ts");
+			route.use("resource.locales", "routes/resource.locales.ts");
+			route.use("robots.txt", "routes/robots[.]txt.ts");
+			route.use("sitemap-index.xml", "routes/sitemap-index[.]xml.ts");
+			route.use("sitemap.$lang.xml", "routes/sitemap.$lang[.]xml.ts");
 		});
 	}
 }
